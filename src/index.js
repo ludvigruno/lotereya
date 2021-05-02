ea0f8ba6806e47579095ee85c397e516e6399b13
ludvigRuno <ludvigruno@yandex.ru>
2021-05-02 13:12:38 +0300

    удалена папка

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import '@babel/polyfill';
 import React from "react";
 import ReactDOM from "react-dom";
 import WindowChat from './components/index'
-
+let i;
 ReactDOM.render(
     <WindowChat />,
     document.querySelector('#contentFortuna')
